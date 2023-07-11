ff7cb79bd29f0bf3356e2b47c4fa7baf2a32b225
KONOSAMA <133698318+nothinglogical@users.noreply.github.com>
2023-07-12 00:08:49 +0530

    My first sum(a+b)

---
+++ sum.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b;
+
+    printf("ENTER NUMBER a\n");
+    scanf("%d",&a);
+
+    printf("ENTER NUMBER b\n");
+    scanf("%d",&b);
+
+    printf("The Sum is %d\n",a+b);
+
+    return 0;
+
+}
