cea29672b6d45baf02dd5d3980cdc32683d4eb39
KONOSAMA <133698318+nothinglogical@users.noreply.github.com>
2023-07-12 21:01:51 +0530

    Two Input (a,b) for four output (+,/,*,-)

---
+++ SumDiveMultiSub.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter Number a\n");
+    scanf("%d",&a);
+
+    printf("Enter Number b\n");
+    scanf("%d",&b);
+
+    print("SUM=%d\n",a+b);
+
+    printf("DIVIE=%d\n",a/b);
+
+    printf("MULTIPLE=%d\n",a*b);
+
+    printf("SUBSTRACT=%d\n",a-b);
+
+    return 0;
+}
