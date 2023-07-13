dd865b092b7a75ba72e25ad58399e906e909b01f
KONOSAMA <133698318+nothinglogical@users.noreply.github.com>
2023-07-13 18:05:18 +0530

    Numbering Alphabets

---
+++ Alphabet'.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main()
+
+{
+    int A;
+
+    printf("Enter the thst represents A = \n");
+    scanf("%d",&A);
+
+    printf("B = %d\n",A+1);
+
+    printf("C = %d\n",A+2);
+
+    printf("D = %d\n",A+3);
+
+    printf("E = %d\n",A+4);
+
+    printf("F = %d\n",A+5);
+
+    printf("G = %d\n",A+6);
+    
+    printf("H = %d\n",A+7);
+
+    printf("I = %d\n",A+8);
+
+    printf("J = %d\n",A+9);
+
+    printf("K = %d\n",A+10);
+
+    printf("L = %d\n",A+11);
+
+    printf("M = %d\n",A+12);
+
+    printf("N = %d\n",A+13);
+
+    printf("O = %d\n",A+14);
+
+    printf("P = %d\n",A+15);
+
+    printf("Q = %d\n",A+16);
+
+    printf("R = %d\n",A+17);
+
+    printf("S = %d\n",A+18);
+
+    printf("T = %d\n",A+19);
+
+    printf("U = %d\n",A+20);
+
+    printf("V = %d\n",A+21);
+
+    printf("W = %d\n",A+22);
+
+    printf("X = %d\n",A+23);
+
+    printf("Y = %d\n",A+24);
+
+    printf("Z = %d\n",A+25);
+
+    return 0;
+}
