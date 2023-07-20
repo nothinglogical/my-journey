e280254e9185cf7940e75081bbcdea1c66f8ecf1
KONOSAMA <133698318+nothinglogical@users.noreply.github.com>
2023-07-20 21:43:11 +0530

    everyting i have learn.1

---
+++ one.c
@@ -0,0 +1,113 @@
+#include<stdio.h>
+int main ()
+{
+
+    printf("Hello,\n");
+    printf("Myself KONO, in this project i am going to perform a small/tinny task with my noobie skills\n");
+    printf("In this project we will perform few calcullations,draw some patterns,etc..\n");
+ 
+
+    int a,b,c,d;
+
+    printf("Enter value for a--\n");
+    scanf("%d",&a);
+
+        printf("Enter value for b--\n");
+        scanf("%d",&b);
+
+            printf("Enter value for c--\n");
+            scanf("%d",&c);
+    
+                printf("Enter value for d--\n");
+                scanf("%d",&d);
+
+
+
+                  printf("sum-0 of a,b = %d\n",a+b);
+                  printf("sum-1 of a,c = %d\n",a+c);
+                  printf("sum-2 of a,d = %d\n",a+d);
+                  printf("sum-3 of b,c = %d\n",b+c);
+                  printf("sum-4 of b,d = %d\n",b+d);
+                  printf("sum-5 of c,d = %d\n",c+d);
+            
+                  printf("sum-6 of a,b,c = %d\n",a+b+c);
+                  printf("sum-7 of a,b,d = %d\n",a+b+d);
+                  printf("sum-8 of a,c,d = %d\n",a+c+d);
+                  printf("sum-9 of b,c,d = %d\n",b+c+d);
+
+                  printf("sum-10 of a,b,c,d = %d\n",a+b+c+d);
+
+
+
+                    printf("sub-0 of a,b = %d\n",a-b);
+                    printf("sub-1 of a,c = %d\n",a-c);
+                    printf("sub-2 of a,d = %d\n",a-d);
+                    printf("sub-3 of b,c = %d\n",b-c);
+                    printf("sub-4 of b,d = %d\n",b-d);
+                    printf("sub-5 of c,d = %d\n",c-d);
+            
+                    printf("sub-6 of a,b,c = %d\n",a-b-c);
+                    printf("sub-7 of a,b,d = %d\n",a-b-d);
+                    printf("sub-8 of a,c,d = %d\n",a-c-d);
+                    printf("sub-9 of b,c,d = %d\n",b-c-d);
+
+                    printf("sub-10 of a,b,c,d = %d\n",a-b-c-d);
+
+
+
+                       printf("multi-0 of a,b = %d\n",a*b);
+                       printf("multi-1 of a,c = %d\n",a*c);
+                       printf("multi-2 of a,d = %d\n",a*d);
+                       printf("multi-3 of b,c = %d\n",b*c);
+                       printf("multi-4 of b,d = %d\n",b*d);
+                       printf("multi-5 of c,d = %d\n",c*d);
+            
+                       printf("multi-6 of a,b,c = %d\n",a*b*c);
+                       printf("multi-7 of a,b,d = %d\n",a*b*d);
+                       printf("multi-8 of a,c,d = %d\n",a*c*d);
+                       printf("multi-9 of b,c,d = %d\n",b*c*d);
+
+                       printf("multi-10 of a,b,c,d = %d\n",a*b*c*d);
+
+
+
+                         printf("div-0 of a,b = %d\n",a/b);
+                         printf("div-1 of a,c = %d\n",a/c);
+                         printf("div-2 of a,d = %d\n",a/d);
+                         printf("div-3 of b,c = %d\n",b/c);
+                         printf("div-4 of b,d = %d\n",b/d);
+                         printf("div-5 of c,d = %d\n",c/d);
+            
+                         printf("div-6 of a,b,c = %d\n",a/b/c);
+                         printf("div-7 of a,b,d = %d\n",a/b/d);
+                         printf("div-8 of a,c,d = %d\n",a/c/d);
+                         printf("div-9 of b,c,d = %d\n",b/c/d);
+
+                         printf("div-10 of a,b,c,d = %d\n",a/b/c/d);
+
+
+
+     printf("0101010101    01          0101010101   010        01  0101       0101010101  01010    \n");
+
+       printf("01       01   01          01           0101       01  01 101     01          01  01   \n");
+
+         printf("01        10  01          01           01 10      01  01   10    01          01   10  \n");
+
+           printf("01       01   01          01           01  01     01  01     1   01          01  01   \n");
+
+             printf("0101010101    01          0101010101   01   10    01  01      1  0101010101  0101     \n");
+
+               printf("01       01   01          01           01    01   01  01     1   01          01 10    \n");
+
+                 printf("01        10  01          01           01     10  01  01   10    01          01  01   \n");
+
+                   printf("010      01   01          01           01      01 01  01  010    01          01   10  \n");
+
+                     printf("0101010101    0101010101  0101010101   01       1001  0101       0101010101  01    01 \n");
+
+
+printf("---------------------------------------------------------------DONE------------------------------------------------\n");
+
+
+    return 0;
+}
